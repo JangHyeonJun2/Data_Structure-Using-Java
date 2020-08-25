8e4a5d664af703579ff63ba018fd145bcbe98bd8
JangHyeonJun2 <epfvkdlxj@naver.com>
2020-08-25 14:27:23 +0900

    스케쥴러 프로그램 만들기 2-1(split()함수 사용하기)

--- src/예제/Scheuler.java
+++ src/예제/Scheuler.java
@@ -6,16 +6,26 @@ public class Scheuler {
 
     public Event[] events = new Event[100];
     public int eventsCount = 0;
-
+    Scanner sc;
 
 
     public void processCommand(){
-        Scanner sc = new Scanner(System.in);
+        sc = new Scanner(System.in);
 
         while (true){
+            System.out.print("$ ");
             String command = sc.next();
             if(command.equals("addevent")){
+                String type = sc.next();
+                if(type.equalsIgnoreCase("oneday")){
+                    handleAddOneDayEvent();
+                }else if (type.equalsIgnoreCase("duration")){
+                    handleAddDurationEvent();
+                }else if (type.equalsIgnoreCase("deadline")){
+                    handelAddDeadlineEvent();
+                }else if(type.equalsIgnoreCase("exit")){
 
+                }
             }else if(command.equals("list")){
 
             }else if(command.equals("show")){
@@ -26,6 +36,25 @@ public class Scheuler {
         }
         sc.close();
     }
+
+    private void handelAddDeadlineEvent() {
+
+    }
+
+    private void handleAddDurationEvent(){
+
+    }
+
+    private void handleAddOneDayEvent(){
+        System.out.print("  when:  ");
+        String dateString = sc.next(); //2014/12/3
+        System.out.print("  title: ");
+        String datetitle = sc.next(); // mybirthday
+
+        String[] dates = dateString.split("/"); // 구분자(/)로 새로운 배열 만듬
+
+    }
+
     public static void main(String[] args) {
         Scheuler app = new Scheuler();
         app.processCommand();
